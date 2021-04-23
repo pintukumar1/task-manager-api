fb251a6600b0a010b403da175c78c5c8fa6a889e
pintukumar1 <kumarpintu1713122@gmail.com>
2021-04-23 09:53:53 +0530

    mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
     useNewUrlParser: true,
@@ -6,32 +7,73 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
     useUnifiedTopology: true
 })
 
-// const User = mongoose.model('User', {
-//     name : {
-//         type: String
-//     },
-//     age : {
-//         type : Number
-//     }
-// })
+const User = mongoose.model('User', {
+    name : {
+        type: String,
+        required: true,
+        trim: true
+    },
+    Email : {
+        type: String,
+        required : true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is invalid")
+            }
+        }
+    },
+    Password : {
+        type : String,
+        required: true,
+        minLength : 7,
+        trim: true,
+        vaidate(value){
+            if(value.toLowerCase().includes("password")){
+                throw new Error("Password cannot contain password")
+            }
+        }
+    },
+    age : {
+        type : Number,
+        default: 0,
+        validate(value){
+            if(value < 0){
+                throw new Error('Age must be a positive number')
+            }
+        }
+    }
+})
 
 // const me = new User({
 //     name: 'Pintu', 
-//     age: 22
+//     age: 22,
+//     Email: 'MYEMAIL@PK.IO',
+//     Password: '             qwerty12                       '
+
+// })
+
+// me.save().then((result) => {
+//     console.log(result)
+// }).catch((error) => {
+//     console.log("Error", error)
 // })
 
 const Tasks = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        trim: true,
+        required: true
     },
     completed: {
-        type : Boolean
+        type : Boolean,
+        default: false
     }
 })
 
 const task = new Tasks({
-    description: "Watering the plants",
-    completed: false
+    description: "      Eat Lunch",
 })
 
 task.save().then(() => {
