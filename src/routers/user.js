6e16f59199516b0620a7d65608eb89a8035dc846
pintukumar1 <kumarpintu1713122@gmail.com>
2021-05-01 16:52:00 +0530

    first commit

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,7 +4,6 @@ const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
-
     try {
         await user.save()
         res.status(201).send(user)
@@ -13,8 +12,16 @@ router.post('/users', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch(e) {
+        res.status(400).send()
+    }
+})
 
+router.get('/users', async (req, res) => {
     try {
         const users = await User.find({})
         res.send(users)
@@ -46,7 +53,12 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true , runValidators: true})
+        const user = await User.findById(req.params.id)
+        updates.forEach((update) => user[update] = req.body[update] )
+
+        await user.save()
+
+        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true , runValidators: true})
         if(!user) {
             res.status(404).send()
         }
