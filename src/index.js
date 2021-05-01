0ba1e814079f3864522d41dee5ee266d04469200
pintukumar1 <kumarpintu1713122@gmail.com>
2021-05-01 12:16:20 +0530

    api bcrypt

--- src/index.js
+++ src/index.js
@@ -14,3 +14,17 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on Port ' + port)
 })
+
+const bcrypt = require('bcryptjs')
+
+ const myFunction = async() => {
+     const password = "Pintu@123"
+     const hashedPassword = await bcrypt.hash(password, 8) 
+     console.log(password)
+     console.log(hashedPassword)
+
+     const isMatch = await bcrypt.compare("Pintu@123", hashedPassword)
+     console.log(isMatch)
+ }
+
+ myFunction()
